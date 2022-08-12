dfef56995464ef79a7ea5708463c8f3630bba452
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2022-08-12 20:16:53 +0200

    No longer return an error on vumeter command.

--- kittyMusic_files/ext_music.c
+++ kittyMusic_files/ext_music.c
@@ -1148,10 +1148,30 @@ char *musicVolume KITTENS_CMD_ARGS
 	return tokenBuffer;
 }
 
+char *_musicVumeter( struct glueCommands *data, int nextToken )
+{
+	struct KittyInstance *instance = data -> instance;
+	int args =__stack - data->stack +1;
+
+	printf("%s:%s:%d\n",__FILE__,__FUNCTION__,__LINE__);
+
+	switch (args)
+	{
+		case 1:
+			printf("NYI: Vumeter\n");
+			return  NULL ;
+			break;
+	}
+
+	api.setError(22,data->tokenBuffer);
+	popStack( instance,__stack - data->stack );
+	return  NULL ;
+}
+
 char *musicVumeter KITTENS_CMD_ARGS
 {
 	printf("%s:%s:%d\n",__FILE__,__FUNCTION__,__LINE__);
-	api.setError(22, tokenBuffer);
+	stackCmdParm( _musicVumeter, tokenBuffer );
 	return tokenBuffer;
 }
 
